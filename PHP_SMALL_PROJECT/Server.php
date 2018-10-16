3ac3219db7c3f0f793e07ef0da59923ad1dbf092
boqianlv <3049344036@qq.com>
2018-10-16 21:15:11 +0800

    表单练习
    （突然有想做个社团/学生会部员信息管理系统的想法）

---
+++ PHP_SMALL_PROJECT/Server.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boqian
+ * Date: 2018/10/16
+ * Time: 20:20
+ */
+    echo "<h1>提交成功！</h1>";
+    echo "职位：".$_POST['Department_work']."<br>学号：".$_POST['Student_id']."<br>姓名：".$_POST['Student_name']."<br>QQ：".$_POST['Student_QQ'];
+
+?>
